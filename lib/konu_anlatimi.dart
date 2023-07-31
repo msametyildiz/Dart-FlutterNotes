d8f03159b97ec572524496c6dc92a8819fc772eb
msametyildiz <97543064+msametyildiz@users.noreply.github.com>
2023-07-31 15:14:41 +0300

    added the new part

--- lib/konu_anlatimi.dart
+++ lib/konu_anlatimi.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(){
  /* var name="samet";
   var age =22;
@@ -56,7 +58,117 @@ void main(){
   3- ) Metinden sayilara donusum
 
   toDouble() , toInt() , toStirng() , int.parse() , double.parse();
-  */
+
+  //Sayılardan sayıya donusum
+  int i=42;
+  double d=42.5;
+
+  int newi=d.toInt();
+  double newd=i.toDouble();
+  print(newi);
+  print(newd);
+
+ //  Sayılardan metine donusum
+  String newtext = i.toString();
+  String newtext2 = d.toString();
+  print(newtext);
+  print(newtext2);
+
+  // Metinden sayilara donusum
+
+  String str = "34";
+  String str2 = "34.56";
+  int num = int.parse(str);
+  double num2 = double.parse(str2);
+  print("new number : $num ");
+  print("new number 2 : $num2 ");
+
+
+
+  // konsol girdisi yapma
+  stdin.readLineSync()! ifadesi string değeri almaktadır .
+
+  print("adinizi girin : ");
+  String name = stdin.readLineSync()!;
+  print("yas gir : ");
+  int num = int.parse(stdin.readLineSync()!);
+  print("boy gir : ");
+  double num2 = double.parse(stdin.readLineSync()!);
+  print("adiniz : $name yasiniz : $num boyunuz : $num2");
+  print("toplam ${num+num2}");
+
+
+
+  //    KARŞILAŞTIRMA OPERATÖRLERİ
+  print("enter first num :");
+  int num1 = int.parse(stdin.readLineSync()!);
+  print("enter second number : ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  double total = num1+num2;
+  if(total>=50 && num1==12){
+    print("passed");
+  }
+  else if(total==30){
+    print("nice");
+  }
+  print(num1 == num2);
+  print(num1 != num2);
+
+  print(total >=50 || total <75);
+  print(num1 <50 && num2 == 20);
+
+
+  //        SWİTCH -CASE
+  print("enter number : (1-3)");
+  int num = int.parse(stdin.readLineSync()!);
+  switch(num){
+    case 1:
+      print("num 1");
+      break;
+    case 2:
+      print("num 2");
+      break;
+    case 3:
+      print("num 3");
+      break;
+    default :
+      print("bro wronge !! ");
+      break;
+  }
+  print("lutfen bir secenek sec\n1-toplama\n2-carpma\n3-cıkarma\n4-bolme\n");
+  int choose= int.parse(stdin.readLineSync()!);
+  print("lutfen ilk sayiyi gir : ");
+  int num1 = int.parse(stdin.readLineSync()!);
+  print("lutfen ikinci sayiyi gir : ");
+  int num2 = int.parse(stdin.readLineSync()!);
+  int result;
+  switch(choose){
+    case 1:
+      result = num1+num2;
+      print("The result is : $result");
+      break;
+    case 2:
+      result = num1*num2;
+      print("The result is : $result");
+      break;
+    case 3:
+      result = num1-num2;
+      print("The result is : $result");
+      break;
+    case 4:
+      result = num1%num2;
+      print("The result is : $result");
+      break;
+    default:
+      print("wronge choose");
+      break;
+
+  }
+*/
+
+
+
 
 
 
